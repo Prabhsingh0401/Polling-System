cf99bc22a9602ffa9dd88ca4e27a6a2365426bf6
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2025-04-03 12:22:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+    res.send("Server is working!");
+});
+
+
 io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
     
